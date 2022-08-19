2caf11a49e902bd7e5cd4f0155204ace9ae601b5
Laura <lmcsay@gmail.com>
2022-08-19 18:26:38 -0500

    Combine current reducers to a root reducer

--- client/src/store.js
+++ client/src/store.js
@@ -1,9 +1,13 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
 
 import userSlice from './features/user/userSlice';
+import alertSlice from './features/alert/alertSlice';
+
+const rootReducer = combineReducers({
+  user: userSlice,
+  alert: alertSlice,
+});
 
 export const store = configureStore({
-  reducer: {
-    user: userSlice,
-  },
+  reducer: rootReducer,
 });
