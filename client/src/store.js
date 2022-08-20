3ddfb1add605758a95a65243c8db0c538e57a463
Laura <lmcsay@gmail.com>
2022-08-20 15:04:17 -0500

    Add job slice to root reducer

--- client/src/store.js
+++ client/src/store.js
@@ -2,10 +2,12 @@ import { combineReducers, configureStore } from '@reduxjs/toolkit';
 
 import userSlice from './features/user/userSlice';
 import alertSlice from './features/alert/alertSlice';
+import jobSlice from './features/job/jobSlice';
 
 const rootReducer = combineReducers({
   user: userSlice,
   alert: alertSlice,
+  job: jobSlice,
 });
 
 export const store = configureStore({
